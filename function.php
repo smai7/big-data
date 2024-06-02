932c69c6e8e41464e4e1f6ca59b642be915eded7
smai7 <148942219+smai7@users.noreply.github.com>
2024-06-02 21:30:00 +0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,64 @@
+<?php
+
+session_start();
+
+$conn = mysqli_connect("localhost","root","","stockbarang");
+
+if(isset($_POST['addnewbarang'])){
+    $namabarang = $_POST['namabarang'];
+    $deskripsi = $_POST['deskripsi'];
+    $stock = $_POST['stock'];
+
+    $addtotable = mysqli_query($conn, "insert into stock (namabarang, deskripsi, stock) values('$namabarang','$deskripsi','$stock')");
+    if($addtodable) {
+        header('location:index.php');
+    } else {
+
+    }
+};
+
+//menambah barang masuk
+if(isset($_POST['barangmasuk'])){
+    $barangnya = $_POST['barangnya'];
+    $penerima = $_POST['penerima'];
+    $qty = $_POST['qty'];
+
+    $cekstocksekarang = mysqli_query($conn,"select * from stock where idbarang='$barangnya'");
+    $ambildatanya = mysqli_fetch_array($cekstocksekarang);
+
+    $stocksekarang = $ambildatanya['stock'];
+    $tambahkanstocksekarangdenganquantity = $stocksekarang+$qty;
+
+    $addtomasuk = mysqli_query($conn, "insert into masuk(idbarang, keterangan, qty) values('$barangnya','$penerima','$qty')");
+    $updatestockmasuk = mysqli_query($conn,"update stock set stock='$tambahkanstocksekarangdenganquantity' where idbarang='$barangnya'");
+    if($addtomasuk&&$updatestockmasuk){
+        header('location:masuk.php');
+    } else {
+        echo 'Gagal';
+        header('location:masuk.php');
+    }
+}
+
+//menambah barang keluar
+if(isset($_POST['addbarangkeluar'])){
+    $barangnya = $_POST['barangnya'];
+    $penerima = $_POST['penerima'];
+    $qty = $_POST['qty'];
+
+    $cekstocksekarang = mysqli_query($conn,"select * from stock where idbarang='$barangnya'");
+    $ambildatanya = mysqli_fetch_array($cekstocksekarang);
+
+    $stocksekarang = $ambildatanya['stock'];
+    $tambahkanstocksekarangdenganquantity = $stocksekarang-$qty;
+
+    $addtokeluar = mysqli_query($conn, "insert into keluar(idbarang, penerima, qty) values('$barangnya','$penerima','$qty')");
+    $updatestockmasuk = mysqli_query($conn,"update stock set stock='$tambahkanstocksekarangdenganquantity' where idbarang='$barangnya'");
+    if($addtomasuk&&$updatestockmasuk){
+        header('location:masuk.php');
+    } else {
+        echo 'Gagal';
+        header('location:masuk.php');
+    }
+}
+
+?>
